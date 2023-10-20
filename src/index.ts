dc333dc5365523eeda1f79fc1ccd188cdce5a2f7
yuorei <108039575+yuorei@users.noreply.github.com>
2023-10-20 20:49:43 +0900

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
-const PORT = 3000
+const PORT = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
