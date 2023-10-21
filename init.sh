acdac9d1d6d8bcd83a0a75a287648328635a61e8
yuorei <108039575+yuorei@users.noreply.github.com>
2023-10-21 14:09:39 +0900

    prisma

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@ npm init -y
 npm install -D typescript @types/node
 npm install express
 npm install -D @types/express
+npm install prisma --save-dev
+npm install prisma typescript ts-node @types/node --save-dev
+npx prisma init
+npm install @prisma/client
