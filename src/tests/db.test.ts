ceebb7c127d533a066d9794da8a606b6cf8ad561
yuorei <108039575+yuorei@users.noreply.github.com>
2023-10-31 22:18:11 +0900

    DBからAllUserテスト

--- src/tests/db.test.ts
+++ src/tests/db.test.ts
@@ -1,5 +1,59 @@
-import { createUser } from './lib/functions-without-context'
+import { getAllUser, createUser } from '../infra/users'
 import { prismaMock } from '../lib/singleton'
+
+test('should get all users', async () => {
+  const users = [
+    {
+      id: 'db_uuid',
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password',
+      rank: 'S',
+      total_achievements: 0,
+      profileImageURL: 'https://example.com/image.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 'db_uuid2',
+      name: 'Jane Doe',
+      email: 'john2@example.com',
+      password: 'password',
+      rank: 'S',
+      total_achievements: 0,
+      profileImageURL: 'https://example.com/image.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]
+
+  prismaMock.user.findMany.mockResolvedValue(users)
+
+  await expect(getAllUser()).resolves.toEqual([
+    {
+      id: 'db_uuid',
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password',
+      rank: 'S',
+      total_achievements: 0,
+      profileImageURL: 'https://example.com/image.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 'db_uuid2',
+      name: 'Jane Doe',
+      email: 'john2@example.com',
+      password: 'password',
+      rank: 'S',
+      total_achievements: 0,
+      profileImageURL: 'https://example.com/image.jpg',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ])
+})
 
 test('should create new user ', async () => {
   let ct = new Date()
