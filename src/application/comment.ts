efc82a77aa2fc66fe7f22ff282aa784ad077458d
yuorei <108039575+yuorei@users.noreply.github.com>
2023-11-09 00:54:17 +0900

    comment CRUD

--- src/application/comment.ts
+++ src/application/comment.ts
@@ -11,3 +11,62 @@ export const getAllComment = async () => {
         throw new Error(`Error in getting all comment: ${error}`);
     }
 }
+
+export const createComment = async (commentInput: Comment) => {
+    commentInput.id = generateUUID();
+
+    try {
+        await commentDB.createComment(commentInput);
+    } catch (error) {
+        console.error("Error in creating comment:", error);
+        throw new Error(`Error in creating comment: ${error}`);
+    }
+}
+
+export const updateComment = async (commentId: string, commentInput: Comment) => {
+    try {
+        await commentDB.updateComment(commentId, commentInput);
+    } catch (error) {
+        console.error("Error in updating comment:", error);
+        throw new Error(`Error in updating comment: ${error}`);
+    }
+}
+
+export const deleteComment = async (commentId: string) => {
+    try {
+        await commentDB.deleteComment(commentId);
+    } catch (error) {
+        console.error("Error in deleting comment:", error);
+        throw new Error(`Error in deleting comment: ${error}`);
+    }
+}
+
+export const getCommentById = async (commentId: string) => {
+    try {
+        const comment = await commentDB.getCommentById(commentId);
+        return comment;
+    } catch (error) {
+        console.error("Error in getting comment by id:", error);
+        throw new Error(`Error in getting comment by id: ${error}`);
+    }
+}
+
+export const getCommentByUserId = async (userId: string) => {
+    try {
+        const comment = await commentDB.getCommentByUserId(userId);
+        return comment;
+    } catch (error) {
+        console.error("Error in getting comment by user id:", error);
+        throw new Error(`Error in getting comment by user id: ${error}`);
+    }
+}
+
+export const getCommentByPostId = async (postId: string) => {
+    try {
+        const comment = await commentDB.getCommentByPostId(postId);
+        return comment;
+    } catch (error) {
+        console.error("Error in getting comment by post id:", error);
+        throw new Error(`Error in getting comment by post id: ${error}`);
+    }
+}
--- src/router/comment.ts
+++ src/router/comment.ts
@@ -6,5 +6,11 @@ import * as commentRouter from "../interface/comment";
 const router = Router();
 // /comment/以下のルーティング
 router.get('/', commentRouter.getAllComment);
+router.get('/:id', commentRouter.getCommentById);
+router.get('/user/', verifyToken, commentRouter.getCommentByUserId);
+router.get('/post/:id', commentRouter.getCommentByPostId);
+router.post('/', verifyToken, commentRouter.createComment);
+router.put('/:id', verifyToken, commentRouter.updateComment);
+router.delete('/:id', verifyToken, commentRouter.deleteComment);
 
 export default router;
