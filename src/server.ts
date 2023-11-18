c38ea5dcccab7b16acf700fc8d034ece4720c203
yuorei <108039575+yuorei@users.noreply.github.com>
2023-11-18 20:19:02 +0900

    add PATCH

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 // CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
   res.header(
     'Access-Control-Allow-Headers',
     'Content-Type, Authorization'
